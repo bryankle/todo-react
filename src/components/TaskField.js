6141f5a3ea217145d1ba612cc4c8e81db255e59c
Bryan <bryan@bryanle.io>
2017-07-19 20:07:24 -0400

    Form outputs key values as pressed - now implementing submit on enter key

--- src/components/TaskField.js
+++ src/components/TaskField.js
@@ -3,12 +3,24 @@ import { FormGroup, FormControl } from 'react-bootstrap';
 // Enter new task into field and hit enter to add task to list
 class TaskField extends Component {
 
+	constructor(props) {
+		super(props)
+		this.state = {
+			newTask: ''
+		}
+	}
+
+	_handleChange = (e) => {
+		this.setState({newTask: e.target.value})
+	}
+
 	render() {
 		return  (
 			<div>
 				<FormGroup bsSize="large">
-			      <FormControl type="text" placeholder="Large text" />
+			      <FormControl onChange={this._handleChange} type="text" placeholder="Large text" />
 			    </FormGroup>
+				{this.state.newTask}
 			</div>
 		)
 	}
